04d6469b26cdbffe75fc4733d3212a0c7bb8f9ab
vaishnavimatre <vaishnavimatre8@gmail.com>
2024-04-05 18:31:13 +0530

    test: Add test commit
    Should not pushed

--- check_name_domain.py
+++ check_name_domain.py
@@ -2,12 +2,12 @@
 import subprocess
 
 
-expectedName = "vaishnavi"
+expectedName = "vaishnavimatre"
 name = subprocess.run(["git", "config", "user.name"], capture_output=True, text=True).stdout.strip()
 
 if name == expectedName:
     print("You are using email as per configuration, proceeding to commit")
     exit(0)
 else:
-    print(f"Configured email is not as per config. It should be {expectedEmail}")
+    print(f"Configured email is not as per config. It should be {expectedName}")
     exit(1)
