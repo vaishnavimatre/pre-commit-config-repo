3448ed1d1531f824022aa452bf931d12e50b5ead
vaishnavimatre <vaishnavimatre8@gmail.com>
2024-04-05 18:26:40 +0530

    feat: Add user class

---
+++ check_name_domain.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import subprocess
+
+
+expectedName = "vaishnavi"
+name = subprocess.run(["git", "config", "user.name"], capture_output=True, text=True).stdout.strip()
+
+if name == expectedName:
+    print("You are using email as per configuration, proceeding to commit")
+    exit(0)
+else:
+    print(f"Configured email is not as per config. It should be {expectedEmail}")
+    exit(1)
---
+++ user.java
@@ -0,0 +1,7 @@
+public class User {
+  String userName;
+  String LoginName;
+  String Password;
+  String Confirm_password;
+  String Userrole;
+}
