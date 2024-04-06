afd4cff2a35d9929bce98baa071592314b0aaa72
vaishnavimatre <vaishnavimatre8@gmail.com>
2024-04-06 21:03:32 +0530

    feat: Add product class

---
+++ check_mail_domain.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+domain="vaishnavimatre8@gmail.com"
+email="$(git config user.email)"
+
+
+if [["$email" == "$domain"]];then 
+    echo "You are using email as per configuration, proceeding to commit"
+    exit 0
+else:
+   echo "Configured email is not as per config. It should be $domain"
+    exit 1
